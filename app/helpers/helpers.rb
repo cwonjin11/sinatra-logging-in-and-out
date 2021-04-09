b8525492e6f38e1ee185bac90662e76c784bc270
Wonjin Cho <cwonjin11@gmail.com>
2021-04-09 09:57:28 -0400

    commit tree d6b2025a5e374a1e324539722ead3c238429fc1f

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+    def self.current_user(session)
+        @user = User.find_by_id(session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        #This method should return true if the user_id is in the session hash and false if not.
+        !!session[:user_id]
+    end
+    
 end
